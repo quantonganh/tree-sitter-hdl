0dff153502e7d29d45db45f46dc7d6c39b7b2998
Quan Tong <quantonganh@gmail.com>
2025-08-01 22:06:00 +0700

    add the grammar rules to parse a simple .hdl file

--- grammar.js
+++ grammar.js
@@ -8,10 +8,80 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "hdl",
+    name: "hdl",
 
-  rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    extras: $ => [
+        /\s/,
+        $.comment
+    ],
+    
+    rules: {
+        source_file: $ => repeat($._definition),
+
+        _definition: $ => $.chip_definition,
+
+        chip_definition: $ => seq(
+            'CHIP',
+            field('name', $.identifier),
+            '{',
+            field('header', $.chip_header),
+            field('body', $.chip_body),
+            '}'
+        ),
+
+        chip_header: $ => seq(
+            $.in_section,
+            $.out_section,
+        ),
+
+        in_section: $ => seq(
+            'IN',
+            commaSep1(field('input_pin_name', $.identifier)),
+            ';'
+        ),
+
+        out_section: $ => seq(
+            'OUT',
+            commaSep1(field('output_pin_name', $.identifier)),
+            ';'
+        ),
+
+        chip_body: $ => seq(
+            'PARTS',
+            ':',
+            repeat($.part),
+        ),
+
+        part: $ => seq(
+            field('chip_name', $.identifier),
+            '(',
+            optional(commaSep1($.connection)),
+            ')',
+            ';'
+        ),
+
+        connection: $ => seq(
+            field('part_pin', $.identifier),
+            '=',
+            field('chip_pin', $.identifier)
+        ),
+
+        identifier_list: $ => seq(
+            $.identifier,
+            repeat(seq(',', $.identifier))
+        ),
+
+        identifier: _ => /[A-Za-z_][A-Za-z0-9_]*/,
+
+        number: _ => /\d+/,
+        
+        comment: _ => token(choice(
+            seq('//', /.*/),
+            seq('/**', repeat(/[^*]|\*[^/]/), '*/')
+        )),
+    }
 });
+
+function commaSep1(rule) {
+    return seq(rule, repeat(seq(',', rule)))
+}
